4820b2cfed4abd7a2d31bc70eb32b1e16b943965
d3vv3 <jaimecondel@gmail.com>
2024-10-17 22:46:12 +0200

    fix: styling and discarding products

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
 
   const switchUseForStats = (index) => {
     const newProducts = [...ticketStatistics.items];
-    newProducts[index].is_food = !newProducts[index].is_food;
+    newProducts[index].product.is_food = !newProducts[index].product?.is_food;
     setTicketStatistics({ ...ticketStatistics, items: newProducts });
   }
 
@@ -80,12 +80,12 @@ export default function Home() {
                   ) : (null)}
                 </div>
               ) : (
-                  <div className="grid grid-cols-9 gap-y-1 w-full text-sm md:tx-base px-2 md:px-4">
+                  <div className="grid grid-cols-9 gap-y-1 sm:max-w-3xl w-full text-sm md:tx-base px-2 md:px-4">
                     <div className="col-span-1"></div>
-                    <div className="font-semibold col-span-4">Descripción</div>
-                    <div className="flex font-semibold justify-center col-span-1">Uds.</div>
-                    <div className="flex font-semibold justify-center col-span-2">Precio ud.</div>
-                    <div className="flex font-semibold justify-center col-span-1">Total</div>
+                    <div className="font-semibold col-span-4 sm:text-xl">Descripción</div>
+                    <div className="flex font-semibold justify-center col-span-1 sm:text-xl">Uds.</div>
+                    <div className="flex font-semibold justify-center col-span-2 sm:text-xl">Precio ud.</div>
+                    <div className="flex font-semibold justify-center col-span-1 sm:text-xl">Total</div>
                     {(ticketStatistics?.items).map((item, index) => (
                       <React.Fragment key={index}>
                         <div className={`flex items-center justify-center col-span-1 ${item.product?.is_food ? "" : "grayscale" }`} onClick={() => switchUseForStats(index)}>
@@ -107,7 +107,7 @@ export default function Home() {
                     ))}
                     <div className="flex items-center col-start-9 font-bold col-span-1">
                       {ticketStatistics.items
-                        .reduce((total, item) => item.product?.is_food ? total + Number(item.price) : total, 0)
+                        .reduce((total, item) => item.product?.is_food ? total + Number(item.total_price) : total, 0)
                         .toFixed(2)}€
                     </div>
                   </div>
