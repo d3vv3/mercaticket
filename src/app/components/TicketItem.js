24921d247ceb09d504f38c06c5451482619e5bad
d3vv3 <jaimecondel@gmail.com>
2024-10-18 18:33:21 +0200

    fix: N/A for non food items

--- src/app/components/TicketItem.js
+++ src/app/components/TicketItem.js
@@ -21,11 +21,11 @@ const TicketItem = ({ item, index, switchUseForStats, dailyKcal = 2000 }) => {
       <div className={`flex items-center justify-center col-span-1 rounded-r-lg border-r-2 border-y-2 ${item.product?.is_food ? "bg-green-50 border-green-700" : "line-through bg-gray-100 mb-2" }`} onClick={() => switchUseForStats(index)}>{Number(item.total_price).toFixed(2)}</div>
       { item.product?.is_food && (
         <div className="flex items-center justify-center col-start-2 col-span-8 border-2 border-x-2 rounded-lg border-amber-200 mb-2">
-          <div className="flex flex-row gap-x-4">
-            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Coste/{dailyKcal} kcal diarias: </span> {item.stats?.cost_per_daily_kcal?.toFixed(2)} €</div>
-            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Proteínas: </span> {item.product?.nutritional_information?.protein?.toFixed(2)}%</div>
-            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Hidratos: </span> {item.product?.nutritional_information?.total_carbohydrate?.toFixed(2)}%</div>
-            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Grasas: </span> {item.product?.nutritional_information?.total_fat?.toFixed(2)}%</div>
+          <div className="flex flex-row text-xs sm:text-sm flex-wrap gap-x-4 p-2 sm:p-2">
+            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Coste/{dailyKcal} kcal diarias: </span> {item.stats?.cost_per_daily_kcal?.toFixed(2) ?? "N/A"} €</div>
+            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Proteínas: </span> {item.product?.nutritional_information?.protein?.toFixed(2) ?? "N/A"}%</div>
+            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Hidratos: </span> {item.product?.nutritional_information?.total_carbohydrate?.toFixed(2) ?? "N/A"}%</div>
+            <div className="flex items-center justify-center col-span-2"><span className="font-bold">Grasas: </span> {item.product?.nutritional_information?.total_fat?.toFixed(2) ?? "N/A"}%</div>
           </div>
         </div>
       )
